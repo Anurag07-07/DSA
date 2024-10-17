c468ac8f434316b94288fdd1518e54adcfdd335a
Anurag07-07 <adityanv*@gmail.com>
2024-10-17 21:42:56 +0530

    first commit

---
+++ src/function_.java
@@ -0,0 +1,14 @@
+public class function_ {
+    public static void sayhi() {
+        System.out.println("Hello EveryOne");
+    }
+
+    public static void name(String name) {
+        System.out.println("Hello "+name);
+    }
+
+    public static void main(String[] args) {
+        sayhi();
+        name("Anurag");
+    }
+}
